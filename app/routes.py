9a77723c9698a47ae61d642bd33514c122347ebf
Mitter <michael.mitter@imba.oeaw.ac.at>
2020-06-30 16:41:41 +0200

    made templates nicer

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -15,5 +15,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for("index"))
     return render_template('login.html', title='Sign In', form=form)
