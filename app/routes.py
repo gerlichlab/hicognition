ba48b4452954223291b004a999321095f980d688
Mitter <michael.mitter@imba.oeaw.ac.at>
2020-06-30 16:36:48 +0200

    added login to server

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 @app.route('/higlass')
 def higlass():
     return render_template("higlass.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
