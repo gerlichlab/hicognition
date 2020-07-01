9ec89ad63d29541694ef7192eba4916af3af1307
Mitter <michael.mitter@imba.oeaw.ac.at>
2020-07-01 12:39:17 +0200

    added trial for formatting of config

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    HIGLASS_URL = "localhost:8888"
