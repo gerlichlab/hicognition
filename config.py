36f03ec6d89639f5b806921c19329cc0e8c1c434
Mittmich <michael_mitter@hotmail.com>
2020-07-03 10:51:31 +0200

    pylint

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+"""Config class for hicognition server."""
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config(object):
+class Config():
+    """Config class for hicognition server."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'eieieiei'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
