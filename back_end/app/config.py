b4133ee6f5c69dadeaf0007fda6fd5b0c2d21479
Mitter <michael.mitter@imba.oeaw.ac.at>
2020-10-08 14:17:48 +0200

    added explanation for binsize on config

--- back_end/app/config.py
+++ back_end/app/config.py
@@ -26,7 +26,7 @@ class Config:
     CORS_EXPOSE_HEADERS = True
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
     WINDOW_SIZES = [100000, 200000, 400000, 800000]
-    BIN_SIZES = [20000, 50000]
+    BIN_SIZES = [20000, 50000]  # In development mode, 10k hogs too much memory
 
 
 class DevelopmentConfig(Config):
