0255f3071bbc1dd9c85a58ee02ff7fe6900a6aee
Mittmich <michael_mitter@hotmail.com>
2021-01-14 17:55:43 +0100

    black formatting

--- back_end/tests/test_tasks_pipeline_bed.py
+++ back_end/tests/test_tasks_pipeline_bed.py
@@ -11,7 +11,6 @@ from app.models import Dataset
 from app.tasks import pipeline_bed
 
 
-
 class TestPipelineBed(LoginTestCase, TempDirTestCase):
     """Tests whether pipelin_bed task calls
     the pipeline steps correctly"""
@@ -21,11 +20,14 @@ class TestPipelineBed(LoginTestCase, TempDirTestCase):
     @patch("app.tasks.io_helpers.sort_bed")
     @patch("app.tasks._set_task_progress")
     @patch("app.tasks.bed_preprocess_pipeline_step")
-    def test_preprocess_bed_pipeline_step_called_correctly(self, mock_bed_pipeline_step,
-                                                                 mock_set_progress,
-                                                                 mock_sort_bed,
-                                                                 mock_convert_bed,
-                                                                 mock_bedpe_preprocess):
+    def test_preprocess_bed_pipeline_step_called_correctly(
+        self,
+        mock_bed_pipeline_step,
+        mock_set_progress,
+        mock_sort_bed,
+        mock_convert_bed,
+        mock_bedpe_preprocess,
+    ):
         """Tests whether preprocess_bed_pipeline_is_called_correctly"""
         # authenticate
         token = self.add_and_authenticate("test", "asdf")
@@ -49,11 +51,19 @@ class TestPipelineBed(LoginTestCase, TempDirTestCase):
         # check whether bed_preprocess was called correctly
         mock_bed_pipeline_step.assert_called_with(1)
         # check whether sort_bed was called correctly
-        mock_sort_bed.assert_called_with(*["/test/path/test3.bed", "/test/path/test3_sorted.bed", self.app.config["CHROM_SIZES"]])
+        mock_sort_bed.assert_called_with(
+            *[
+                "/test/path/test3.bed",
+                "/test/path/test3_sorted.bed",
+                self.app.config["CHROM_SIZES"],
+            ]
+        )
         # check whether convert_bed_to_bedpe and bedpe_preprocess_pipline_step was called correctly
         for window in self.app.config["WINDOW_SIZES"]:
             target_file = "/test/path/test3_sorted.bed" + f".{window}" + ".bedpe"
-            mock_convert_bed.assert_any_call(*["/test/path/test3_sorted.bed", target_file, window])
+            mock_convert_bed.assert_any_call(
+                *["/test/path/test3_sorted.bed", target_file, window]
+            )
             mock_bedpe_preprocess.assert_any_call(*[target_file, 1, window])
         # check whether set_progress was called with 100 last
         mock_set_progress.assert_called_with(100)
