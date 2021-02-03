0727c344037aa9d8bf3c6bcdbd6cbfa00d946e5c
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-02-03 16:50:55 +0100

    added tests for preprocessing public datasets

--- back_end/tests/test_api_preprocess_dataset.py
+++ back_end/tests/test_api_preprocess_dataset.py
@@ -138,6 +138,62 @@ class TestPreprocessDataset(LoginTestCase, TempDirTestCase):
         )
         self.assertEqual(response.status_code, 400)
 
+    @patch("app.models.User.launch_task")
+    def test_pipeline_pileup_is_called_correctly_for_public_unowned_dataset(self, mock_launch):
+        """Tests whether cooler pipeline to do pileups is called correctly."""
+        # authenticate
+        token = self.add_and_authenticate("test", "asdf")
+        token_headers = self.get_token_header(token)
+        # add datasets
+        dataset1 = Dataset(
+            dataset_name="test1",
+            file_path="/test/path/1",
+            higlass_uuid="asdf1234",
+            filetype="cooler",
+            public=True,
+            user_id=2
+        )
+        dataset2 = Dataset(
+            dataset_name="test2",
+            file_path="/test/path/2",
+            higlass_uuid="fdsa4321",
+            filetype="cooler",
+            public=True,
+            user_id=2
+        )
+        dataset3 = Dataset(
+            dataset_name="test3",
+            file_path="/test/path/3",
+            higlass_uuid="fdsa8765",
+            filetype="bedfile",
+            user_id=2
+        )
+        db.session.add(dataset1)
+        db.session.add(dataset2)
+        db.session.add(dataset3)
+        db.session.commit()
+        data = {
+            "dataset_id": "1",
+            "interval_ids": "[1, 2, 3, 4]",
+            "binsizes": "[10000, 20000, 40000]",
+        }
+        # dispatch post request
+        response = self.client.post(
+            "/api/preprocess/",
+            data=data,
+            headers=token_headers,
+            content_type="multipart/form-data",
+        )
+        self.assertEqual(response.status_code, 200)
+        # check whether pipeline has been called with right parameters
+        mock_launch.assert_called_with(
+            "pipeline_pileup",
+            "run pileup pipeline",
+            1,
+            [10000, 20000, 40000],
+            [1, 2, 3, 4],
+        )
+
 
 if __name__ == "__main__":
     res = unittest.main(verbosity=3, exit=False)
