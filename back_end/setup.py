734e8cc6b67b285efc51a9f5b4ed9834cc410fb5
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2021-02-25 15:20:25 +0100

    Update version

--- back_end/setup.py
+++ back_end/setup.py
@@ -1,8 +1,14 @@
 import setuptools
+import os
+
+#with open(os.path.join(os.pardir, 'VERSION')) as version_file:
+#   version_number = version_file.read().strip()
+# TODO why does this not work?
 
 setuptools.setup(
     name="hicognition",
-    version="0.1.0",
+    version = "0.2",
+    #version = version_number
     packages=["hicognition"],
     install_requires=["pandas", "bioframe", "pandas", "requests"],
     classifiers=[
