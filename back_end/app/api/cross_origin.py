712120f61b5cc8f098090a7e6c2140900f358de4
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2021-02-25 21:54:38 +0100

    Addd bigwigWidget

--- back_end/app/api/cross_origin.py
+++ back_end/app/api/cross_origin.py
@@ -3,7 +3,7 @@ from . import api
 
 @api.after_request
 def after_request(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Origin", "*") #TODO finer control
     response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
     response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
     return response
