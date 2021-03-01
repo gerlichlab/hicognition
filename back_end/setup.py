b252c89e9cff2371bc72d4a8b0de75334a5016eb
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2021-03-02 00:55:10 +0100

    Make backend versions depend on VERSIONS file

--- back_end/setup.py
+++ back_end/setup.py
@@ -1,14 +1,14 @@
 import setuptools
 import os
 
-#with open(os.path.join(os.pardir, 'VERSION')) as version_file:
-#   version_number = version_file.read().strip()
-# TODO why does this not work?
+# This does not work if it is in the parent dir
+with open('VERSION') as version_file:
+   version_number = version_file.read().strip()
+
 
 setuptools.setup(
     name="hicognition",
-    version = "0.2",
-    #version = version_number
+    version = version_number,
     packages=["hicognition"],
     install_requires=["pandas", "bioframe", "pandas", "requests"],
     classifiers=[
