4536cbee9784d507578b487f883c9e0681c4890f
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-03-31 17:54:45 +0200

    added cli command for user creation

--- back_end/hicognition_server.py
+++ back_end/hicognition_server.py
@@ -1,5 +1,7 @@
 """Start hicognition server."""
 import os
+from getpass import getpass
+import click
 from app import create_app, db
 from app.models import (
     User,
@@ -10,10 +12,35 @@ from app.models import (
     BedFileMetadata,
 )
 from flask_migrate import Migrate
+from flask.cli import AppGroup
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
 migrate = Migrate(app, db)
 
+# add command line arguments for user creation
+
+user_group = AppGroup("user")
+@user_group.command('define')
+@click.argument('name')
+@click.option('--password','-p', default=None)
+def create_user(name, password):
+    """Creates a new user either with defined password or password prompt.
+    If user with the name exists already, password is redefined."""
+    # check if user with such a name exists
+    if User.query.filter(User.username == name).first() is not None:
+        # if user exists, get the user
+        user = User.query.filter(User.username == name).first()
+    else:
+        # otherwise make a new one
+        user = User(username=name)
+    # prompt for password if not defined
+    if password is None:
+        password = getpass(f"Enter password for {name}: ")
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+app.cli.add_command(user_group)
 
 @app.shell_context_processor
 def make_shell_context():
