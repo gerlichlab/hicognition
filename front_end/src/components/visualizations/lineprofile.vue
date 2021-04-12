a243a1b69721fac54f821f22ad4093c3156fb6ec
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2021-04-12 11:43:42 +0200

    Fixed linechart

--- front_end/src/components/visualizations/lineprofile.vue
+++ front_end/src/components/visualizations/lineprofile.vue
@@ -13,12 +13,12 @@
 <script>
 import * as d3 from "d3";
 
-function range(start,end){
-    var ans =[];
-    for (let i =start; i<=end;i++){
+function range(start, end) {
+    var ans = [];
+    for (let i = start; i <= end; i++) {
         ans.push(i);
     }
-    return ans
+    return ans;
 }
 
 export default {
@@ -37,19 +37,20 @@ export default {
             return "lineprofile_" + this.lineprofileID;
         },
         lineData: function() {
-            return{
+            return {
                 y: this.lineprofileData.data,
-                x: range(1,this.lineprofileData.data.length)
+                x: range(1, this.lineprofileData.data.length)
             };
-        },
+        }
     },
     methods: {
         redrawLinechart: function() {
             var margin = { top: 10, right: 30, bottom: 30, left: 60 };
-            console.log(this.lineData)
+            console.log(this.lineData.y);
+            //console.log(Math.max.apply(Math, this.lineData.y));
             d3.select(`#${this.lineprofileDivID}Svg`).remove();
-            var svg = d3
-                .select(`#${this.lineprofileDivID}Svg`)
+            var line_svg = d3
+                .select(`#${this.lineprofileDivID}`)
                 .append("svg")
                 .attr("id", `${this.lineprofileDivID}Svg`)
                 .attr("width", this.width + margin.left + margin.right)
@@ -60,39 +61,95 @@ export default {
                     "translate(" + margin.left + "," + margin.top + ")"
                 );
             // Add X axis
+
+            let minX = d3.min(this.lineData.x);
+            let maxX = d3.max(this.lineData.x);
+            let minY = d3.min(this.lineData.y);
+            let maxY = d3.max(this.lineData.y);
+
             var x = d3
                 .scaleLinear()
-                .domain([1, 100])
+                .domain([minX, maxX])
                 .range([0, this.width]);
-            svg.append("g")
-                .attr("transform", "translate(0," + this.height + ")")
-                .call(d3.axisBottom(x));
-
-            // Add Y axis
             var y = d3
                 .scaleLinear()
-                .domain([0, 13])
+                .domain([minY-0.05*maxY, maxY])
                 .range([this.height, 0]);
-            svg.append("g").call(d3.axisLeft(y));
+            let x_test = this.lineData.x;
+            let y_test = this.lineData.y;
+            var line = d3
+                .line()
+                .x(function(d, i) {
+                    return x(x_test[i]);
+                })
+                .y(function(d, i) {
+                    return y(y_test[i]);
+                });
+            // var x = d3
+            //     .scaleLinear()
+            //     .domain([0, this.lineprofileData.data.length])
+            //     .range([0, this.width]);
+            // svg.append("g")
+            //     .attr("transform", "translate(0," + this.height + ")")
+            //     .call(d3.axisBottom(x));
 
-            // Add the line
-            svg.append("path")
-                .datum(this.lineData)
+            // Add Y axis
+            // var y = d3
+            //     .scaleLinear()
+            //     // ... is the short version for Math.max.apply(Math, this.lineData.y)
+            //     .domain([0, Math.max(...this.lineData.y)])
+            //     .range([this.height, 0]);
+            // svg.append("g").call(d3.axisLeft(y));
+
+            let g = line_svg.append("g");
+            var xAxis = d3.axisBottom().scale(x);
+            g.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0," + this.height + ")")
+                .call(xAxis);
+
+            var yAxis = d3.axisLeft().scale(y);
+            g.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0,0)")
+                .call(yAxis);
+
+            g.append("path")
+                .attr("d", line(this.lineData.x))
                 .attr("fill", "none")
                 .attr("stroke", "steelblue")
-                .attr("stroke-width", 1.5)
-                .attr(
-                    "d",
-                    d3
-                        .line()
-                        .x(function(d) {
-                            return x(d.x);
-                        })
-                        .y(function(d) {
-                            return y(d.y);
-                        })
-                );
+                .attr("stroke-width", 1.5);
+
+            // var line = d3.svg
+            //     .line()
+            //     .x(function(d, i) {
+            //         return x(d.x[i]);
+            //     })
+            //     .y(function(d, i) {
+            //         return y(d.y[i]);
+            //     });
+
+            // Add the line
+            // svg.append("path")
+            //     .datum(this.lineData)
+            //     .attr("fill", "none")
+            //     .attr("stroke", "steelblue")
+            //     .attr("stroke-width", 1.5)
+            //     .attr(
+            //         "d",
+            //         d3
+            //             .line()
+            //             .x(function(d, i) {
+            //                 return x(d.x[i]);
+            //             })
+            //             .y(function(d, i) {
+            //                 return y(d.y[i]);
+            //             })
+            //     );
         }
+    },
+    mounted: function() {
+        this.redrawLinechart();
     },
     watch: {
         height: function() {
