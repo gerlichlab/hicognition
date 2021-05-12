a3b05b0ef7b74b9359d2007fd3472d1f4a1ff878
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-05-12 14:36:39 +0200

    tests pass for spliting pipeline tasks

--- back_end/tests/test_tasks_pipeline_stackup.py
+++ back_end/tests/test_tasks_pipeline_stackup.py
@@ -13,73 +13,6 @@ from app.tasks import pipeline_stackup
 from app.pipeline_steps import perform_stackup
 
 
-class TestPipelineStackup(LoginTestCase, TempDirTestCase):
-    """Tests whether pipelin_stackup task calls
-    the pipeline steps correctly"""
-
-    def setUp(self):
-        """Add test dataset"""
-        # call setUp of LoginTestCase to initialize app
-        super(TestPipelineStackup, self).setUp()
-        # authenticate
-        token = self.add_and_authenticate("test", "asdf")
-        # create token_header
-        token_headers = self.get_token_header(token)
-        # add content-type
-        token_headers["Content-Type"] = "multipart/form-data"
-        # add dataset
-        self.dataset = Dataset(
-            dataset_name="test3",
-            file_path="/test/path/test3.bw",
-            filetype="bigwig",
-            processing_state="finished",
-            user_id=1,
-        )
-        # add intervals
-        self.intervals1 = Intervals(
-            name="testRegion1",
-            dataset_id=1,
-            file_path="test_path_1.bedd2db",
-            windowsize=200000,
-        )
-        self.intervals2 = Intervals(
-            name="testRegion2",
-            dataset_id=1,
-            file_path="test_path_2.bedd2db",
-            windowsize=200000,
-        )
-        db.session.add(self.dataset)
-        db.session.add(self.intervals1)
-        db.session.add(self.intervals2)
-        db.session.commit()
-
-    @patch("app.pipeline_steps._set_task_progress")
-    @patch("app.pipeline_steps.perform_stackup")
-    def test_pipeline_stackup_calls_steps_correctly(
-        self, mock_perform_stackup, mock_set_progress
-    ):
-        """Tests whether the functions that execute the different pipeline steps are called
-        correctly."""
-        # launch task
-        binsizes = [10000, 20000, 30000]
-        dataset_id = 1
-        intervals_id = [1, 2]
-        intervals_objects = [self.intervals1, self.intervals2]
-        pipeline_stackup(dataset_id, binsizes, intervals_id)
-        # compare expected call arguments with actual call arguments
-        for binsize in binsizes:
-            for intervals in intervals_objects:
-                # check whether the current combination is in call args list
-                expected_call_args = [self.dataset, intervals, binsize]
-                mock_perform_stackup.assert_any_call(*expected_call_args)
-        # check whether number of calls was as expected
-        self.assertEqual(
-            mock_perform_stackup.call_count, len(binsizes) * len(intervals_objects)
-        )
-        # check whether last call to set task progress was 100
-        mock_set_progress.assert_called_with(100)
-
-
 class TestPerformStackup(LoginTestCase, TempDirTestCase):
     def setUp(self):
         """Add test dataset"""
@@ -140,7 +73,9 @@ class TestPerformStackup(LoginTestCase, TempDirTestCase):
         )
         mock_read_csv.return_value = test_df_interval
         # dispatch call
-        perform_stackup(self.dataset, self.intervals1, 10000)
+        dataset_id = 1
+        intervals_id = 1
+        perform_stackup(dataset_id, intervals_id, 10000)
         # check whether stackup was called correctly
         mock_stackup.assert_called_with(
             self.dataset.file_path,
@@ -164,7 +99,9 @@ class TestPerformStackup(LoginTestCase, TempDirTestCase):
         test_df_interval = pd.DataFrame({0: ["chr1", "chr1"], 1: [500, 1500]})
         mock_read_csv.return_value = test_df_interval
         # dispatch call
-        perform_stackup(self.dataset, self.intervals1, 10000)
+        dataset_id = 1
+        intervals_id = 1
+        perform_stackup(dataset_id, intervals_id, 10000)
         # check whether stackup was called correctly
         mock_stackup.assert_called_with(
             self.dataset.file_path,
@@ -190,8 +127,10 @@ class TestPerformStackup(LoginTestCase, TempDirTestCase):
         )
         mock_read_csv.return_value = test_df_interval
         # dispatch call
+        dataset_id = 2
+        intervals_id = 2
         perform_stackup(
-            self.dataset2, self.intervals2, 50000
+            dataset_id, intervals_id, 50000
         )  # interfls2 has a windowsize of 50kb, with binsize of 50kb, this will produce 2 values per example
         # check whether example is correct
         file_path = IndividualIntervalData.query.get(
@@ -216,8 +155,10 @@ class TestPerformStackup(LoginTestCase, TempDirTestCase):
         )
         mock_read_csv.return_value = test_df_interval
         # dispatch call
+        dataset_id = 2
+        intervals_id = 2
         perform_stackup(
-            self.dataset2, self.intervals2, 50000
+            dataset_id, intervals_id, 50000
         )  # interfls2 has a windowsize of 50kb, with binsize of 50kb, this will produce 2 values per example
         # check whether example is correct
         file_path = IndividualIntervalData.query.get(
@@ -243,8 +184,10 @@ class TestPerformStackup(LoginTestCase, TempDirTestCase):
         )
         mock_read_csv.return_value = test_df_interval
         # dispatch call
+        dataset_id = 2
+        intervals_id = 2
         perform_stackup(
-            self.dataset2, self.intervals2, 50000
+            dataset_id, intervals_id, 50000
         )  # interfls2 has a windowsize of 50kb, with binsize of 50kb, this will produce 2 values per example
         # check whether example is correct
         file_path = IndividualIntervalData.query.get(
@@ -271,8 +214,10 @@ class TestPerformStackup(LoginTestCase, TempDirTestCase):
         )
         mock_read_csv.return_value = test_df_interval
         # dispatch call
+        dataset_id = 2
+        intervals_id = 2
         perform_stackup(
-            self.dataset2, self.intervals2, 50000
+            dataset_id, intervals_id, 50000
         )  # interfls2 has a windowsize of 50kb, with binsize of 50kb, this will produce 2 values per example
         # check whether example is correct
         file_path = IndividualIntervalData.query.get(
