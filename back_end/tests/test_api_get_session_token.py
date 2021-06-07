d63f9990817c926f08e80712827d7282f65de385
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-06-07 13:38:00 +0200

    refactored get session token

--- back_end/tests/test_api_get_session_token.py
+++ back_end/tests/test_api_get_session_token.py
@@ -14,17 +14,26 @@ class TestGetSessionToken(LoginTestCase):
     """Tests for /api/sessions/id/sessionToken route to list
     datasets."""
 
-    def create_sessions(self):
+    def setUp(self):
+        super().setUp()
         # define datasets
         self.timestamp = datetime.datetime.utcnow()
-        self.session_user_1 = Session(user_id=1, name="test", created_utc=self.timestamp)
-        self.session_user_1_2 = Session(user_id=1, name="test2", created_utc=self.timestamp)
-        self.session_user_2 = Session(user_id=2, name="test3", created_utc=self.timestamp)
+        self.session_user_1 = Session(
+            user_id=1, name="test", created_utc=self.timestamp
+        )
+        self.session_user_1_2 = Session(
+            user_id=1, name="test2", created_utc=self.timestamp
+        )
+        self.session_user_2 = Session(
+            user_id=2, name="test3", created_utc=self.timestamp
+        )
 
     def test_no_auth(self):
         """No authentication provided, response should be 401"""
         # protected route
-        response = self.client.get("/api/sessions/1/sessionToken/", content_type="application/json")
+        response = self.client.get(
+            "/api/sessions/1/sessionToken/", content_type="application/json"
+        )
         self.assertEqual(response.status_code, 401)
 
     def test_session_does_not_exist(self):
@@ -42,7 +51,6 @@ class TestGetSessionToken(LoginTestCase):
 
     def test_unowned_session(self):
         """Existing session is not owned."""
-        self.create_sessions()
         token = self.add_and_authenticate("test", "asdf")
         # create token_headers
         token_headers = self.get_token_header(token)
@@ -59,7 +67,6 @@ class TestGetSessionToken(LoginTestCase):
 
     def test_valid_token_for_owned_session(self):
         """Existing session is not owned."""
-        self.create_sessions()
         token = self.add_and_authenticate("test", "asdf")
         # create token_headers
         token_headers = self.get_token_header(token)
@@ -78,11 +85,11 @@ class TestGetSessionToken(LoginTestCase):
         result = Session.verify_auth_token(token)
         self.assertEqual(result, self.session_user_1)
 
-
     def test_invalid_token_not_accepted(self):
         """Invalid token is not accepted by session"""
         result = Session.verify_auth_token("asdf")
         self.assertTrue(result is None)
+
 
 if __name__ == "__main__":
     res = unittest.main(verbosity=3, exit=False)
