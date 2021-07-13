77b42e5561e72c9624b3154d32676fd214ebe348
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-07-13 11:08:59 +0200

    added nicer formatting to add dataset dialog

--- front_end/src/components/dialogs/addDatasetDialog.vue
+++ front_end/src/components/dialogs/addDatasetDialog.vue
@@ -9,9 +9,51 @@
                             >Information about addition of data</span
                         >
                     </div>
-                    <div v-for="(item, index) in infoText" :key="index">
-                        <span class="md-subheading">{{ item }}</span>
+                    <div class="mainText">
+                        <p>
+                            <span class="md-subheading">
+                                Here you can add datasets. Datasets can either
+                                be genomic intervals or genomic features.
+                                Genomic intervals can be added int form of
+                                bedfiles and genomic features can be added
+                                either in the form of cooler files (.mcool) or
+                                bigwig files.
+                            </span>
+                        </p>
+                        <br />
+                        <p>
+                            <span class="md-subheading">
+                                The added files need to conform to certain
+                                formatting standards to ensure compatibility.
+                                For bedfiles, the first three columns need to be
+                                <b>(chrom, start, end)</b> and specify the
+                                chromosome, start-position and end-position of a
+                                feature. In addition, chromosomes need to be in
+                                the hg19 genome and not be any of the small
+                                contigs (only chr${specifier} is accepted, where
+                                specified is either a number from 1 to 22 or one
+                                of X or Y).
+                            </span>
+                        </p>
+                        <br />
+                        <p>
+                            <span class="md-subheading">
+                                For mcooler files, the following resolutions
+                                need to be available:
+                                <b
+                                    >[1000, 2000, 5000, 10000,20000,50000,
+                                    100000, 200000]</b
+                                >. For bigwig files, no formatting requirement
+                                is enforced other than one should be able to
+                                open them with pybbi. Note however that if a
+                                binsize is not availalbe, the respective plot
+                                will not show any data.
+                            </span>
+                        </p>
                     </div>
+                    <!-- <div v-for="(item, index) in infoText" :key="index">
+                        <span class="md-subheading">{{ item }}</span>
+                    </div> -->
                 </md-tooltip>
             </md-dialog-title>
             <addDatasetForm
@@ -26,13 +68,6 @@ import addDatasetForm from "../forms/addDatasetForm";
 
 export default {
     name: "AddDatasetDialog",
-    data: function() {
-        return {
-            infoText: " \nHere you can add datasets. \n Datasets can either be genomic intervals in the case of bedfiles, \n or genomic features, if you add bigwig-files or cooler files. \n\n Looking forward to your data!".split(
-                "\n"
-            )
-        };
-    },
     components: {
         addDatasetForm
     },
@@ -50,6 +85,14 @@ export default {
 <style lang="scss" scoped>
 .md-dialog /deep/.md-dialog-container {
     max-width: 1000px;
+}
+
+.mainText {
+    display: block;
+    width: 20vw;
+    word-wrap: break-word;
+    word-break: break-all;
+    white-space: normal;
 }
 
 .md-tooltip {
