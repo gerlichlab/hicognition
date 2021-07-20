36941f30804a0a4270ac2ac68d3b64df3300c8d6
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-07-20 15:05:02 +0200

    added UI drawer update

--- front_end/src/components/ui/drawer.vue
+++ front_end/src/components/ui/drawer.vue
@@ -1,58 +1,76 @@
 <template>
     <div>
-        <md-toolbar class="md-transparent" md-elevation="0"
-            >Manage datasets</md-toolbar
+        <md-toolbar class="md-accent" md-elevation="3"
+            ><span class="md-title">Data management</span></md-toolbar
         >
-        <md-list>
-            <md-list-item>
-                <md-button
-                    class="md-icon-button"
-                    @click="$emit('add-feature-click')"
-                >
-                    <md-icon>add</md-icon>
-                </md-button>
-                <span class="md-list-item-text">Add Genomic Feature</span>
+
+        <md-list :md-expand-single="true">
+
+
+            <md-list-item md-expand>
+                <span class="md-list-item-text">Manage Datasets</span>
+                <md-list slot="md-expand">
+                    <md-list-item class="md-inset">
+                        <md-button
+                            class="md-icon-button"
+                            @click="$emit('add-feature-click')"
+                        >
+                            <md-icon>add</md-icon>
+                        </md-button>
+                        <span class="md-list-item-text">Add Genomic Feature</span>
+                    </md-list-item>
+
+                    <md-list-item class="md-inset">
+                        <md-button
+                            class="md-icon-button"
+                            @click="$emit('add-region-click')"
+                        >
+                            <md-icon>add</md-icon>
+                        </md-button>
+                        <span class="md-list-item-text">Add Genomic Region</span>
+                    </md-list-item>
+
+                    <md-list-item class="md-inset">
+                        <md-button
+                            class="md-icon-button"
+                            @click="$emit('add-metadata-click')"
+                        >
+                            <md-icon>highlight_alt</md-icon>
+                        </md-button>
+                        <span class="md-list-item-text">Add Metadata</span>
+                    </md-list-item>
+
+                    <md-list-item class="md-inset">
+                        <md-button
+                            class="md-icon-button"
+                            @click="$emit('preprocess-dataset-click')"
+                        >
+                            <md-icon>launch</md-icon>
+                        </md-button>
+                        <span class="md-list-item-text">Preprocess</span>
+                    </md-list-item>
+
+                    <md-list-item class="md-inset">
+                        <md-button
+                            class="md-icon-button"
+                            @click="$emit('mydataset-click')"
+                        >
+                            <md-icon>list</md-icon>
+                        </md-button>
+                        <span class="md-list-item-text">My Datasets</span>
+                    </md-list-item>
+                </md-list>
             </md-list-item>
 
-            <md-list-item>
-                <md-button
-                    class="md-icon-button"
-                    @click="$emit('add-region-click')"
-                >
-                    <md-icon>add</md-icon>
-                </md-button>
-                <span class="md-list-item-text">Add Genomic Region</span>
+            <md-list-item md-expand>
+                <span class="md-list-item-text">Manage Collections</span>
+                <md-list slot="md-expand">
+                    <md-list-item class="md-inset">World</md-list-item>
+                    <md-list-item class="md-inset">Europe</md-list-item>
+                    <md-list-item class="md-inset">South America</md-list-item>
+                </md-list>
             </md-list-item>
 
-            <md-list-item>
-                <md-button
-                    class="md-icon-button"
-                    @click="$emit('add-metadata-click')"
-                >
-                    <md-icon>highlight_alt</md-icon>
-                </md-button>
-                <span class="md-list-item-text">Add Metadata</span>
-            </md-list-item>
-
-            <md-list-item>
-                <md-button
-                    class="md-icon-button"
-                    @click="$emit('preprocess-dataset-click')"
-                >
-                    <md-icon>launch</md-icon>
-                </md-button>
-                <span class="md-list-item-text">Preprocess</span>
-            </md-list-item>
-
-            <md-list-item>
-                <md-button
-                    class="md-icon-button"
-                    @click="$emit('mydataset-click')"
-                >
-                    <md-icon>list</md-icon>
-                </md-button>
-                <span class="md-list-item-text">My Datasets</span>
-            </md-list-item>
         </md-list>
     </div>
 </template>
