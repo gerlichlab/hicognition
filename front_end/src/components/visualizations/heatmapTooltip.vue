b0f64d53a14012dc467ec04fea7d6dd33c73e5cf
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-10-19 11:01:16 +0200

    fixed thumbnail handling

--- front_end/src/components/visualizations/heatmapTooltip.vue
+++ front_end/src/components/visualizations/heatmapTooltip.vue
@@ -98,6 +98,15 @@ export default {
             this.minHeatmapRange = data[2]
             this.maxHeatmapRange = data[3]
         },
+        resetColorScale: function () {
+            /*
+                resets colorscale to undefined
+            */
+            this.minHeatmap = undefined;
+            this.maxHeatmap = undefined;
+            this.minHeatmapRange = undefined;
+            this.maxHeatmapRange = undefined;
+        },
         handleSubmission: function(){
             // check whether there is a name
             if (this.newRegionName.length === 0) {
@@ -132,6 +141,9 @@ export default {
         },
         showControls: function(){
             this.newRegionName = `${this.regionName}-${this.datasetName}: cluster ${this.clusterID}`
+        },
+        thumbnail: function(){
+            this.resetColorScale()
         }
     }
 };
