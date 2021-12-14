688c0131c4d963063d23bf90fba0aa3229dfd433
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2021-12-14 14:18:00 +0100

    added timeout to cypress exit code

--- front_end/e2e/init.sh
+++ front_end/e2e/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wait-on http://hicognition:5000/api/test &&\
-wait-on http://nginx/static/finished.json &&\
+wait-on --interval 1000 http://hicognition:5000/api/test &&\
+wait-on --interval 1000 http://nginx/static/finished.json &&\
 cypress run
