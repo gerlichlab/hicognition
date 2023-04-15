f329aa6bb6b7d48e8d501fb5385bdf3160ac5c80
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2023-04-15 16:55:41 +0200

    added metadata step 3

--- front_end/src/components/forms/step3BulkDatasetForm.vue
+++ front_end/src/components/forms/step3BulkDatasetForm.vue
@@ -1,42 +1,44 @@
 <template>
-    <b-form @submit.prevent="validateDataset">
-          <b-row v-for="(element, index) in elements" :key="index" class="mb-3">
+    <b-container>
+          <b-row
+              v-for="element in elements"
+              :key="element.id"
+          >
             <b-col cols="3">
               <b-form-group label="Dataset Name">
-                <b-form-input v-model="element.datasetName" disabled></b-form-input>
+                <b-form-input v-model="element.datasetName"></b-form-input>
               </b-form-group>
             </b-col>
-            <b-col cols="3">
+            <b-col cols="2">
               <b-form-group label="Celltype">
-                <b-form-input v-model="element.cellType" required></b-form-input>
+                <b-form-input v-model="element.cellType"></b-form-input>
               </b-form-group>
             </b-col>
-            <b-col cols="3">
+            <b-col cols="2">
               <b-form-group label="Perturbation">
-                <b-form-input v-model="element.perturbation" required></b-form-input>
+                <b-form-input v-model="element.perturbation"></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col cols="5">
+              <b-form-group label="Description">
+                <b-form-input v-model="element.description"></b-form-input>
               </b-form-group>
             </b-col>
           </b-row>
-    </b-form>
+    </b-container>
 </template>
 
 <script>
 export default {
   props: {
-    fileInformation: Array,
-    fileTypeMapping: Object,
+    fileInformation: Array
   },
   data() {
     return {
-      elements: [],
-      datasetMetadataMapping: undefined,
+      elements: []
     };
   },
   methods: {
-    getFileType(filename) {
-      let fileEnding = filename.split(".").pop();
-      return this.fileTypeMapping[fileEnding];
-    },
     clearForm() {
       this.elements = [];
       this.initializeFields();
@@ -78,31 +80,32 @@ export default {
       }
     },
     initializeFields() {
-      this.elements = [];
+      this.elements = this.file
       for (let id of Object.keys(this.fileInformation)) {
         let tempObject = {
           id: id,
           datasetName: this.fileInformation[id].datasetName,
           assembly: this.fileInformation[id].assembly,
-          file: this.fileInformation[id].file,
-          public: this.fileInformation[id].public,
           sizeType: this.fileInformation[id].sizeType,
+          file: this.fileInformation[id].file,
+          filename: this.fileInformation[id].name,
+          public: this.fileInformation[id].public,
           perturbation: null,
           cellType: null,
           state: undefined,
+          description: undefined
         };
         this.elements.push(tempObject);
       }
     },
   },
   mounted() {
-    this.datasetMetadataMapping = this.$store.getters["getDatasetMetadataMapping"]["DatasetType"];
     this.initializeFields();
   },
   watch: {
     fileInformation(val) {
       if (val) {
-        this.initializeFields();
+        this.elements = this.fileInformation;
       }
     },
   },
